548d438e17d18a1f77f34f5950c597c22a65b2e2
Brady Simmons <bradysimmons@bradys-air.lan>
2024-10-21 19:08:34 -0500

    Initial commit

---
+++ Dex3/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  Dex3
+//
+//  Created by Brady Simmons on 10/7/24.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
+        animation: .default)
+    private var pokedex: FetchedResults<Pokemon>
+
+    @StateObject private var pokemonVM = PokemonViewModel(controller: FetchController())
+    
+    var body: some View {
+        switch pokemonVM.status {
+        case .success:
+            NavigationStack {
+                List(pokedex) { pokemon in
+                    NavigationLink(value: pokemon) {
+                        AsyncImage(url: pokemon.sprite) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 100, height: 100)
+                        
+                        Text(pokemon.name!.capitalized)
+                    }
+                }
+                .navigationTitle("Pokedex")
+                .navigationDestination(for: Pokemon.self, destination: { pokemon in
+                    PokemonDetail()
+                        .environmentObject(pokemon)
+                })
+                
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        EditButton()
+                    }
+                }
+            }
+        default:
+            ProgressView()
+        }
+    }
+}
+
+
+
+#Preview {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}
---
+++ Dex3/PokemonDetail.swift
@@ -0,0 +1,80 @@
+//
+//  PokemonDetail.swift
+//  Dex3
+//
+//  Created by Brady Simmons on 10/17/24.
+//
+
+import SwiftUI
+import CoreData
+
+struct PokemonDetail: View {
+    @EnvironmentObject var pokemon: Pokemon
+    @State var showShiny = false
+    
+    var body: some View {
+        ScrollView {
+            ZStack {
+                Image(pokemon.background)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: .black, radius: 6)
+                
+                AsyncImage(url: showShiny ? pokemon.shiny : pokemon.sprite) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top, 50)
+                        .shadow(color: .black, radius: 6)
+                    
+                    
+                } placeholder: {
+                    ProgressView()
+                }
+            }
+            
+            HStack {
+                ForEach(pokemon.types!, id: \.self) { type in
+                    Text(type.capitalized)
+                        .font(.title2)
+                        .shadow(color: .white, radius: 1)
+                        .padding([.top, .bottom], 7)
+                        .padding([.leading, .trailing])
+                        .background(Color(type.capitalized))
+                        .cornerRadius(50)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            
+            Text("Stats")
+                .font(.title)
+                .padding(.bottom, -7)
+            Stats()
+                .environmentObject(pokemon)
+        }
+        .navigationTitle(pokemon.name!.capitalized)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    showShiny.toggle()
+                } label: {
+                    if showShiny {
+                        Image(systemName: "wand.and.stars")
+                            .foregroundColor(.yellow)
+                    } else {
+                        Image(systemName: "wand.and.stars.inverse")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    
+    PokemonDetail()
+        .environmentObject(SamplePokemon.samplePokemon)
+    
+}
